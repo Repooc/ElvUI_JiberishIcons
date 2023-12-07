ee710c54ddb0887e4ef9397a39b55f6075e2116b
Repooc <tjcooper1982@gmail.com>
2023-12-07 14:25:57 -0500

    Update texture sprite sheet to match details class icons sheet where it can be used as custom icons

--- ElvUI_JiberishIcons/Init.lua
+++ ElvUI_JiberishIcons/Init.lua
@@ -6,12 +6,13 @@ Engine.iconStyles = {
 
 Engine.iconMinSize = '1'
 Engine.iconMaxSize = '128'
-Engine.classIconPath = [[|TInterface\AddOns\ElvUI_JiberishIcons\Media\Icons\%s:%s:%s:0:0:512:512:%s|t]]
+Engine.classIconPath = [[|TInterface\AddOns\ElvUI_JiberishIcons\Media\Icons\%s:%s:%s:0:0:1024:1024:%s|t]]
 Engine.classIcons = {
 	WARRIOR		= '0:128:0:128',
 	MAGE		= '128:256:0:128',
 	ROGUE		= '256:384:0:128',
 	DRUID		= '384:512:0:128',
+	EVOKER		= '512:640:0:128',
 	HUNTER		= '0:128:128:256',
 	SHAMAN		= '128:256:128:256',
 	PRIEST		= '256:384:128:256',
@@ -20,5 +21,4 @@ Engine.classIcons = {
 	DEATHKNIGHT = '128:256:256:384',
 	MONK		= '256:384:256:384',
 	DEMONHUNTER = '384:512:256:384',
-	EVOKER		= '0:128:384:512',
 }
