c7111c88e52330d6ac92d08e527b0bd4ff8c1976
Repooc <tjcooper1982@gmail.com>
2024-09-08 13:57:25 -0400

    add mMediaTag support

--- ElvUI_JiberishIcons/Core/Core.lua
+++ ElvUI_JiberishIcons/Core/Core.lua
@@ -32,6 +32,7 @@ function JI:Init(event, addon)
 
 			JI:SetupDetails()
 			JI:SetupBlizzardFrames() --* Setup class icon icon for frames
+			JI:Setup_mMediaTag()
 			JI:SetupSUF()
 
 			JI:RegisterChatCommand('ji', 'ToggleOptions')
