e4ba276eb32338a9a5727e3e0b112c978248e714
Blinkii <92640275+mBlinkii@users.noreply.github.com>
2024-09-20 18:11:19 +0100

    Simplification (#4)
    * simplification
    
    i have simplified the function, since i use the same texture format for my icons, it is easier for everyone if the coordinates are only once in the db.
    
    * Update mMediaTag.lua
    
    no need for commented out old code and swap to single quotes for non locales (my style pref is all)
    
    ---------
    
    Co-authored-by: Repooc <tjcooper1982@gmail.com>

--- ElvUI_JiberishIcons/Core/mMediaTag.lua
+++ ElvUI_JiberishIcons/Core/mMediaTag.lua
@@ -3,13 +3,8 @@ local classInfo = JI.icons.class
 
 function JI:Setup_mMediaTag()
 	if not JI:IsAddOnEnabled('ElvUI_mMediaTag') then return end
-	local texCoords = {}
-	for className, data in next, classInfo.data do
-		texCoords[className] = data.texCoords
-	end
-
 	for iconStyle, data in next, classInfo.styles do
 		local path = format('%s%s', classInfo.path, iconStyle)
-		mMT:AddClassIcons(iconStyle, path, texCoords, data.name)
+		mMT:AddClassIcons(iconStyle, path, 'default', data.name)
 	end
 end
