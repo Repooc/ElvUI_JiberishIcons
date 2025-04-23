261fed6be0b1fb5a300aa2d79f1fb0e1ecad8537
Repooc <tjcooper1982@gmail.com>
2025-04-23 19:44:22 -0400

    [fix] copy/paste failure

--- ElvUI_JiberishIcons/Core/mMediaTag.lua
+++ ElvUI_JiberishIcons/Core/mMediaTag.lua
@@ -3,9 +3,9 @@ local path, isCustom
 
 function JI:Setup_mMediaTag()
 	if not JI:IsAddOnEnabled('ElvUI_mMediaTag') then return end
-	local mergedStylePacks = JI.mergedStylePacks.class
+	local mergedClassStyles = JI.mergedStylePacks.class
 
-	for iconStyle, data in next, mergedStylePacks.styles do
+	for iconStyle, data in next, mergedClassStyles.styles do
 		local path = (mergedClassStyles.styles[iconStyle] and mergedClassStyles.styles[iconStyle].path) or mergedClassStyles.path
 		local fullPath = format('%s%s', path, iconStyle)
 		if JI:IsValidTexturePath(fullPath) then
