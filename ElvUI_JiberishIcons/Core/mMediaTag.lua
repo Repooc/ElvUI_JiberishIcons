a2724cb7e0729d0705d719f8c50349361301ad70
Repooc <tjcooper1982@gmail.com>
2025-06-12 12:42:39 -0400

    adding some debug prints for testing something for jib

--- ElvUI_JiberishIcons/Core/mMediaTag.lua
+++ ElvUI_JiberishIcons/Core/mMediaTag.lua
@@ -6,10 +6,14 @@ function JI:Setup_mMediaTag()
 	local mergedClassStyles = JI.mergedStylePacks.class
 
 	for iconStyle, data in next, mergedClassStyles.styles do
+		JI:Print('Attempting to add '..data.name..' to mMediaTag.')
 		local path = (mergedClassStyles.styles[iconStyle] and mergedClassStyles.styles[iconStyle].path) or mergedClassStyles.path
 		local fullPath = format('%s%s', path, iconStyle)
 		if JI:IsValidTexturePath(fullPath) then
 			mMT:AddClassIcons(iconStyle, fullPath, 'default', data.name)
+			JI:Print('Added '..data.name..' to mMediaTag.')
+		else
+			JI:Print('Failed to add '..data.name..' to mMediaTag with style key of '..iconStyle..'.\nTexture Path: '..fullPath)
 		end
 	end
 end
