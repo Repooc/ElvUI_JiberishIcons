f109b09d3c6c1377340f048d9b74995dfb6b7301
Repooc <tjcooper1982@gmail.com>
2025-07-07 23:49:48 -0400

    change it so the details dropdown shows an example from the style instead of default

--- ElvUI_JiberishIcons/Core/Details.lua
+++ ElvUI_JiberishIcons/Core/Details.lua
@@ -9,7 +9,7 @@ function JI:SetupDetails()
 		local path = (mergedClassStyles.styles[iconStyle] and mergedClassStyles.styles[iconStyle].path) or mergedClassStyles.path
 		local fullPath = format('%s%s', path, iconStyle)
 		if JI:IsValidTexturePath(fullPath) then
-            _G.Details:AddCustomIconSet(fullPath, format('%s (Class)', data.name), false)
+            _G.Details:AddCustomIconSet(fullPath, format('%s (Class)', data.name), false, fullPath, { 0.125, 0, 0.125, 0.125, 0.25, 0, 0.25, 0.125 }, {16, 16})
         end
     end
 end
